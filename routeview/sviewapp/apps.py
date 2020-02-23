8a8a9b3e39be6375acff5c3043654c02870ff954
hlions <bavdu>
2020-02-23 13:06:10 +0800

    chapterone

--- routeview/sviewapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SviewappConfig(AppConfig):
-    name = 'sviewapp'
--- website/chapterone/chapterone/asgi.py
+++ website/chapterone/chapterone/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for routeview project.
+ASGI config for chapterone project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'routeview.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chapterone.settings')
 
 application = get_asgi_application()
